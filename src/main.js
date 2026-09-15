d2ce7312a64ac0e0184188d79b6e4a4472ef3b55
DP <dpkds161@gmail.com>
2026-09-15 17:41:47 +0300

    add else to end

--- src/main.js
+++ src/main.js
@@ -53,6 +53,11 @@ form.addEventListener('submit', async event => {
 
     if (data.totalHits > 15) {
       showLoadMoreButton();
+    } else {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
     }
   } catch (error) {
     showErrorNotification();
